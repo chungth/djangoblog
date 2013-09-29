1371599850fa0fc5b1791b4d78d0432142dd3e72
chung tran <huychungtran@gmail.com>
2013-09-29 19:38:39 +0900

    fix setting for polls and admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,7 @@
 # Django settings for mysite project.
 import os
+
+
 APP_DIR = os.path.dirname( globals()['__file__'] )
 
 DBNAME = 'blog'
@@ -16,11 +18,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'blog',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'blog',
+        'PASSWORD': '1',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -121,10 +123,12 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-
+    'django.contrib.admin',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mysite.blog',
+    'polls',
+    
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.contrib import admin
+admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'mysite.blog.views.index'),
     url(r'^update/', 'mysite.blog.views.update'),
     url(r'^delete/', 'mysite.blog.views.delete'),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
